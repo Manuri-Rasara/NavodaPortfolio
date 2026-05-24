64818f6a5700338312b691072a8b8143e32fb1b4
Manuri-Rasara <manurirasarahewage@gmail.com>
2026-05-24 22:10:16 +0530

    mt change again for testonomials

--- myComponent/Testonomials.tsx
+++ myComponent/Testonomials.tsx
@@ -2,7 +2,7 @@ import { Testimonial } from "@/components/design-testimonial"
 
 export default function Page() {
   return (
-    <main className="min-h-screen flex items-center justify-center w-full px-10  mt-10">
+    <main className="min-h-screen flex items-center justify-center w-full px-10  mt-20">
       <Testimonial />
     </main>
   )
